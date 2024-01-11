418aab34529873ccf1dde77f5630bcf3d1089a22
Mark Hempel <mark@hma.co.uk>
2024-01-11 21:35:50 +0000

    initial commit and functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,104 @@
-import Image from 'next/image'
+'use client';
+
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const availableOptions = [
+    {
+      name: 'Rock',
+      slug: 'rock',
+      beats: ['scissors'],
+      loses: ['paper'],
+    },
+    {
+      name: 'Paper',
+      slug: 'paper',
+      beats: ['rock'],
+      loses: ['scissors'],
+    },
+    {
+      name: 'Scissors',
+      slug: 'scissors',
+      beats: ['paper'],
+      loses: ['rock'],
+    },
+  ];
+
+  const [computerScore, setComputerScore] = useState(0);
+  const [playerScore, setPlayerScore] = useState(0);
+
+  const [messageVisible, setMessageVisible] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const btnPress = (value) => {
+    setMessageVisible(false);
+    let randomNumber = Math.floor(Math.random() * availableOptions.length);
+    const computerChoice = availableOptions[randomNumber];
+    const playerWinner = winnerCheck(value, computerChoice);
+  };
+
+  const winnerCheck = (playerValue, computerValue) => {
+    if (playerValue.slug === computerValue.slug) {
+      const text = renderGameplayText(
+        `You chose: ${playerValue.name}\n\n The computer chose: ${computerValue.name}\n\nIt's a Tie!`
+      );
+      return `You chose: ${playerValue.name} \n\n The computer chose ${computerValue.name}\n\nIt's a Tie!`;
+    }
+    if (computerValue.loses.includes(playerValue.slug)) {
+      setPlayerScore(playerScore + 1);
+      renderGameplayText(
+        `You chose: ${playerValue.name}\n\n The computer chose: ${computerValue.name}\n\nYou win, ${playerValue.name} beats ${computerValue.name}`
+      );
+      return `You win, ${playerValue.name} beats ${computerValue.name}`;
+    }
+    if (playerValue.loses.includes(computerValue.slug)) {
+      setComputerScore(computerScore + 1);
+      renderGameplayText(
+        `You chose: ${playerValue.name} \n\n The computer chose ${computerValue.name}\n\nYou lose, ${computerValue.name} beats ${playerValue.name}`
+      );
+      return `You lose, ${computerValue.name} beats ${playerValue.name}`;
+    }
+  };
+
+  const renderGameplayText = (text) => {
+    setMessage(text);
+    setMessageVisible(true);
+  };
+
+  const restartGame = () => {
+    setMessageVisible(false);
+    setMessage('');
+    setComputerScore(0);
+    setPlayerScore(0);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1>Rock, Paper, Scissors Game</h1>
+      <div className='flex-1'>
+        <div className='my-5 justify-between'>
+          <h2 className='my-5'>Choose from the options below!</h2>
+          {availableOptions.map((choice) => (
+            <button
+              className='mx-5'
+              key={choice.slug}
+              onClick={() => btnPress(choice)}
+            >
+              {choice.name}
+            </button>
+          ))}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {messageVisible && (
+        <div className='flex-grow'>
+          <h3 style={{ whiteSpace: 'pre-wrap' }} className='text-xl'>{message}</h3>
+        </div>
+      )}
+      <div>
+        <h3>Computer Score: {computerScore}</h3>
+        <h3>Player Score: {playerScore}</h3>
+        <button onClick={restartGame}>Restart Game</button>
       </div>
     </main>
-  )
+  );
 }
